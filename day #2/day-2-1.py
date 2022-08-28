66f5b740ffc63c37a6e841ed316bcdefd2113dc3
ali mosaad <ali.mosaad50@hotmail.com>
2022-08-28 14:34:05 +0200

    "add day-2-1"

---
+++ day #2/day-2-1.py
@@ -0,0 +1,10 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+f_digit=int(two_digit_number[0])
+l_digit=int(two_digit_number[1])
+out_put=f_digit+l_digit
+print(out_put)
